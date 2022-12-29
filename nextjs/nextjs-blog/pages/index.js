0e3025093d47381d5992f4f99888c11e80dba028
kongdefei <kongdefei@bytedance.com>
2022-12-29 11:02:23 +0800

    add nextjs example

--- nextjs/nextjs-blog/pages/index.js
+++ nextjs/nextjs-blog/pages/index.js
@@ -4,17 +4,25 @@ import Date from '../components/date';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
-
+import { NotionAPI } from 'notion-client'
+import { NotionRenderer } from 'react-notion-x'
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
+
+  const notion = new NotionAPI()
+
+  const recordMap = await notion.getPage('067dd719a912471ea9a3ac10710e7fdf')
+
   return {
     props: {
       allPostsData,
+      recordMap,
     },
   };
+
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData, recordMap }) {
   return (
     <Layout home>
       <Head>
@@ -36,6 +44,8 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
       </section>
+
+      <NotionRenderer recordMap={recordMap} fullPage={true} darkMode={false} />
     </Layout>
   );
 }
